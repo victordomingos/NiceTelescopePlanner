b9729103712bc45eddef8b779211a1e3d4fafa64
Victor Domingos <editor.arcosonline@gmail.com>
2019-09-28 05:51:43 +0100

    ...

--- NiceTelescopePlanner/src/main/java/gui/Main.java
+++ NiceTelescopePlanner/src/main/java/gui/Main.java
@@ -579,7 +579,8 @@ public class Main extends javax.swing.JFrame {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    //javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel("Metal");
                     break;
                 }
             }
