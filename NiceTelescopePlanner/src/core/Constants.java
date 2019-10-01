6f9177544786eb6c9ee7acbcf78ab76645733848
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-01 02:53:18 +0100

    extract panel

---
+++ NiceTelescopePlanner/src/core/Constants.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author victor
+ */
+public class Constants {
+    public static final String[] OUR_TOP_LIST_DEEPSPACE = {
+            "NGC104",  "NGC224",  "NGC253",  "NGC598",  "NGC869",  "NGC884", 
+            "NGC1976", "NGC2070", "NGC3293", "NGC3372", "NGC4594", "NGC5128", 
+            "NGC5139", "NGC5236", "NGC6121", "NGC6205", "NGC6405", "NGC6514", 
+            "NGC6533", "NGC6611", "NGC6618", "NGC6656", "NGC6720", "NGC6853", 
+            "NGC7089", 
+    };
+//    
+//    public static final DEFAULT_LOCATION = {
+//        'latitude': 41.55926513671875,
+//        'longitude': -8.405625509894655,
+//        'altitude': 190.0,
+//        'city': 'Braga',
+//        'country': 'Portugal',
+//        'dms_latitude': '41°33\'33.38\" N',
+//        'dms_longitude': '8°24\'20.5\" W',
+//    };
+    
+    
+    
+    
+    /*
+    Limiting Magnitude values:
+    -27 to +22
+     
+    Moon:   -12.70
+    Sun:    -26.74
+    Venus:   -4.80
+    Mars:    -2.94
+    Jupiter  -2.94
+    Sirius:  -1.46
+    Betelgeuse:  0
+    
+     +6.5  Faintest star / naked eye (average)
+     +8    Extreme naked eye limit
+     +9.5  7x50 Binoculars
+    +14.5  Most common telescopes
+    +22    24" Telescope 30min stacked images
+    
+    
+    */
+}
