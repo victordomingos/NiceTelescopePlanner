cbe411fff2297fe13f1371d7339429c84d10a62b
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-10 17:50:21 +0100

    api exploration

--- NiceTelescopePlanner/test/tests/scratchpad.java
+++ NiceTelescopePlanner/test/tests/scratchpad.java
@@ -25,8 +25,14 @@ import jparsec.ephem.Ephem;
 import jparsec.ephem.EphemerisElement;
 import jparsec.ephem.RiseSetTransit;
 import jparsec.ephem.Target;
+import jparsec.ephem.Target.TARGET;
 import jparsec.ephem.planets.EphemElement;
+import jparsec.io.ConsoleReport;
+import static jparsec.io.ConsoleReport.fullEphemReportToConsole;
+import static jparsec.math.Constant.DEG_TO_RAD;
+import static jparsec.math.Constant.RAD_TO_DEG;
 import jparsec.observer.ObserverElement;
+import jparsec.time.AstroDate;
 import jparsec.time.TimeElement;
 import jparsec.util.JPARSECException;
 
@@ -41,67 +47,89 @@ public class scratchpad {
         TimeElement timeEl = null;
         EphemerisElement ephemerisEl = null;
         EphemElement ephemEl = null;
+        AstroDate astrodt = null;
         
-        loc = new Location("India", 0.23741713814878865, 1.3514957062818092, 686);
-        
+        loc = new Location("AVV", 42*DEG_TO_RAD , -8.5*DEG_TO_RAD, 194);
+    System.out.println("=======> LOCATION: " + loc);
+
+    
         ObserverElement observer = new ObserverElement(loc.getName(), 
                 loc.getLatitudeRad(), loc.getLongitudeRad(), loc.getHeight(), 
-                loc.getTimeZoneOffset());
+                0);
+    System.out.println("=======> OBS-ELEMENT: " + observer);
+
+    
+
+        astrodt = new AstroDate(2019, AstroDate.OCTOBER, 11, 23, 0, 0);
+        timeEl = new TimeElement(astrodt, TimeElement.SCALE.LOCAL_TIME);
+
+        //timeEl = new TimeElement("2019-10-10 00:00:00 UTC");
         
-        try {
-            timeEl = new TimeElement("2019-10-10 00:00:00");
-        } catch (JPARSECException ex) {
-            System.out.println(ex);
-        }
+    System.out.println("=======> TIME-ELEMENT: " + timeEl);
+    
+    
         
-        
-        System.out.println("Available targets: ");
+        System.out.println("Available targets: -----------------------");
         int i = 0;
         for (String name : Target.getNames() ){
-            System.out.print(name + " \t\t\t");
+            System.out.print(name + ", ");
             i++;
-            if(i%5 == 0) {
+            if(i%6 == 0) {
                 System.out.println("");
             }
         }
-        
+        System.out.println("------------------------------------------");
         System.out.println("Total targets: " + Target.getNames().length);
         
-        try {
-            ephemerisEl = new EphemerisElement(Target.getID("Saturn"),
-                    EphemerisElement.COORDINATES_TYPE.APPARENT,
-                    EphemerisElement.EQUINOX_OF_DATE, true,
-                    EphemerisElement.REDUCTION_METHOD.IAU_2006,
-                    EphemerisElement.FRAME.ICRF);
-        } catch (JPARSECException ex) {
-            System.out.println(ex);
-        }
+        
+//            ephemerisEl = new EphemerisElement(Target.getID("Saturn"),
+//                    EphemerisElement.COORDINATES_TYPE.APPARENT,
+//                    EphemerisElement.EQUINOX_J2000, true,
+//                    EphemerisElement.REDUCTION_METHOD.IAU_2006,
+//                    EphemerisElement.FRAME.ICRF);
+        ephemerisEl = new EphemerisElement(TARGET.SATURN, 
+                EphemerisElement.COORDINATES_TYPE.APPARENT, 
+                EphemerisElement.EQUINOX_OF_DATE, EphemerisElement.TOPOCENTRIC, 
+                EphemerisElement.REDUCTION_METHOD.IAU_2006, 
+                EphemerisElement.FRAME.DYNAMICAL_EQUINOX_J2000, 
+                EphemerisElement.ALGORITHM.MOSHIER);
         
         
+        System.out.println("=======> EPHEMERIS-ELEMENT: " + ephemerisEl);
         
-        try {
-            ephemEl = Ephem.getEphemeris("Saturn", timeEl, observer, ephemerisEl, true);
-        } catch (JPARSECException ex) {
-            System.out.println(ex);
-        }
+        ephemEl = Ephem.getEphemeris(timeEl, observer, ephemerisEl, true);
+        System.out.println("=======> EPHEM-ELEMENT: " + ephemEl);
         
-        
-        System.out.println("LOCATION: " + loc);
-        System.out.println("");
-        System.out.println("OBS-ELEMENT: " + observer);
-        System.out.println("");
-        System.out.println("TIME-ELEMENT: " + timeEl);
-        System.out.println("");
-        System.out.println("EPHEMERIS-ELEMENT: " + ephemerisEl);
-        System.out.println("");
-        System.out.println("EPHEM-ELEMENT: " + ephemEl);
+        //System.out.println("=======> LOCATION: " + loc);
+        //System.out.println("");
+        //System.out.println("=======> OBS-ELEMENT: " + observer);
+        //System.out.println("");
+        //System.out.println("=======> TIME-ELEMENT: " + timeEl);
+        //System.out.println("");
+        //System.out.println("=======> EPHEMERIS-ELEMENT: " + ephemerisEl);
+        //System.out.println("");
+        //System.out.println("=======> EPHEM-ELEMENT: " + ephemEl);
         
         System.out.println("===========================");
         
         EphemElement rise;
-        rise = RiseSetTransit.obtainCurrentOrNextRiseSetTransit(timeEl, 
+        rise = RiseSetTransit.obtainNextRiseSetTransit(timeEl, 
                 observer, ephemerisEl, ephemEl,
-                RiseSetTransit.TWILIGHT.TWILIGHT_CIVIL);
+                RiseSetTransit.TWILIGHT.TWILIGHT_ASTRONOMICAL);
+        System.out.println("");
+        System.out.println("=======> RISE"
+                + "\n  Alt: " + rise.elevation * RAD_TO_DEG
+                + "\n  Az: " + rise.azimuth * RAD_TO_DEG
+                + "\n Constellation: " + rise.constellation
+                + "\n Magnitude: " + rise.magnitude
+                + "\n Phase: " + rise.phase * 100 + "%"
+                + "\n Phase Angle: " + rise.phaseAngle * RAD_TO_DEG + "˚"
+        );
+        
+        System.out.println(ConsoleReport.getFullEphemReport(ephemEl, false, false, 4));
+        
+        TimeElement time = new TimeElement(astro, TimeElement.SCALE.BARYCENTRIC_DYNAMICAL_TIME);
+        EphemElement ephem = Ephem.getEphemeris(time, observer, eph, true);
         
         System.out.println();
         
