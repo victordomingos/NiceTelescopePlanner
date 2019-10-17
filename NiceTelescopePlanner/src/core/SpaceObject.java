4941b2dd7ef0ef6c01f07bb4044e8e84d1f98143
Victor Domingos <editor.arcosonline@gmail.com>
2019-10-18 00:51:37 +0100

    ...

--- NiceTelescopePlanner/src/core/SpaceObject.java
+++ NiceTelescopePlanner/src/core/SpaceObject.java
@@ -201,8 +201,8 @@ public class SpaceObject {
     public boolean willBeAboveHorizon() throws JPARSECException {
         for (TimeElement t : getHourlyTimeElements()) {
             ephemEl = Ephem.getEphemeris(t, this.observer, ephemerisEl, true);
-            //get object elevation for specified time
-            if (ephemEl.elevation * RAD_TO_DEG > 0) { //not rise here.
+            //get object elevation for specified instant
+            if (ephemEl.elevation * RAD_TO_DEG > 0) { 
                 return true;
             }
         }
@@ -255,62 +255,6 @@ public class SpaceObject {
         return time_interval;
     }
 
-    /**
-     * Determine if target will be above horizon during the specified interval
-     *
-     * @return
-     */
-    public boolean willBeAboveHorizon_old() throws JPARSECException {
-
-        double jdUT_start = TimeScale.getJD(startTimeEl, observer, ephemerisEl,
-                SCALE.UNIVERSAL_TIME_UT1);
-        double jdUT_end = TimeScale.getJD(endTimeEl, observer, ephemerisEl,
-                SCALE.UNIVERSAL_TIME_UT1);
-
-        System.out.println(name + " - Rises:    " + rises.size());
-        System.out.println(name + " - Transits: " + transits.size());
-        System.out.println(name + " - Sets:     " + sets.size());
-
-        if (sets.get(0) == RiseSetTransit.ALWAYS_BELOW_HORIZON) {
-            System.out.println(name + ": Never up in the horizon.");
-            return false;
-        }
-
-        //Is the first rise between start and end?
-        // Or is the last set between start and end?
-        for (double rise : rises) {
-            if (jdUT_start < rise && rise < jdUT_end) {
-                System.out.println("\n\n" + name + ": Is the first rise between start and end? "
-                        + (jdUT_start < rises.get(0)));
-                System.out.println(name + ": Is the last rise between start and end?  "
-                        + (jdUT_end < rises.get(rises.size() - 1)));
-                return true;
-            }
-        }
-
-        for (double set : sets) {
-            if (jdUT_start < set && set < jdUT_end) {
-                System.out.println(name + ": Is the first set between start and end? "
-                        + (jdUT_start < sets.get(0)));
-                System.out.println(name + ": Is the last set between start and end?  "
-                        + (jdUT_end < sets.get(sets.size() - 1)));
-                return true;
-            }
-        }
-
-        for (double transit : transits) {
-            if (jdUT_start < transit && transit < jdUT_end) {
-                System.out.println(name + ": Is the first transit between start and end? "
-                        + (jdUT_start < transits.get(0)));
-                System.out.println(name + ": Is the last transit between start and end?  "
-                        + (jdUT_end < transits.get(transits.size() - 1)));
-                return true;
-            }
-        }
-
-        System.out.println("\n" + name + ": wont be up on the horizon.\n\n\n");
-        return false;
-    }
 
     /**
      * Determine visibility at a given time according to estimated limiting
--- NiceTelescopePlanner/test/tests/simpletime.java
+++ NiceTelescopePlanner/test/tests/simpletime.java
@@ -18,6 +18,7 @@ package tests;
 
 import jparsec.ephem.Ephem;
 import jparsec.ephem.EphemerisElement;
+import jparsec.ephem.RiseSetTransit;
 import jparsec.ephem.Target.TARGET;
 import jparsec.ephem.planets.EphemElement;
 import jparsec.io.ConsoleReport;
@@ -67,3 +68,62 @@ public class simpletime {
     }
 
 }
+
+
+//
+//    /**
+//     * Determine if target will be above horizon during the specified interval
+//     *
+//     * @return
+//     */
+//    public boolean willBeAboveHorizon_old() throws JPARSECException {
+//
+//        double jdUT_start = TimeScale.getJD(startTimeEl, observer, ephemerisEl,
+//                TimeElement.SCALE.UNIVERSAL_TIME_UT1);
+//        double jdUT_end = TimeScale.getJD(endTimeEl, observer, ephemerisEl,
+//                TimeElement.SCALE.UNIVERSAL_TIME_UT1);
+//
+//        System.out.println(name + " - Rises:    " + rises.size());
+//        System.out.println(name + " - Transits: " + transits.size());
+//        System.out.println(name + " - Sets:     " + sets.size());
+//
+//        if (sets.get(0) == RiseSetTransit.ALWAYS_BELOW_HORIZON) {
+//            System.out.println(name + ": Never up in the horizon.");
+//            return false;
+//        }
+//
+//        //Is the first rise between start and end?
+//        // Or is the last set between start and end?
+//        for (double rise : rises) {
+//            if (jdUT_start < rise && rise < jdUT_end) {
+//                System.out.println("\n\n" + name + ": Is the first rise between start and end? "
+//                        + (jdUT_start < rises.get(0)));
+//                System.out.println(name + ": Is the last rise between start and end?  "
+//                        + (jdUT_end < rises.get(rises.size() - 1)));
+//                return true;
+//            }
+//        }
+//
+//        for (double set : sets) {
+//            if (jdUT_start < set && set < jdUT_end) {
+//                System.out.println(name + ": Is the first set between start and end? "
+//                        + (jdUT_start < sets.get(0)));
+//                System.out.println(name + ": Is the last set between start and end?  "
+//                        + (jdUT_end < sets.get(sets.size() - 1)));
+//                return true;
+//            }
+//        }
+//
+//        for (double transit : transits) {
+//            if (jdUT_start < transit && transit < jdUT_end) {
+//                System.out.println(name + ": Is the first transit between start and end? "
+//                        + (jdUT_start < transits.get(0)));
+//                System.out.println(name + ": Is the last transit between start and end?  "
+//                        + (jdUT_end < transits.get(transits.size() - 1)));
+//                return true;
+//            }
+//        }
+//
+//        System.out.println("\n" + name + ": wont be up on the horizon.\n\n\n");
+//        return false;
+//    }
